f6a130ed33aba9b60bfe980c9e3e79529a06cd13
zayennn <zaayeennn@gmail.com>
2025-10-27 09:47:55 +0700

    update

--- bundles/nasted_loop.js
+++ bundles/nasted_loop.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// latihan 1
 for (let i = 1; i <= 3; i++) {
     console.log(`baris ke-${i}`);
     for (let j = 1; j <= 3; j++) {
